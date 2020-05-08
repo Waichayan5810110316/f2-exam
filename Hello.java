d5bf928f80f0028787bb99027dbc001bcbc0d595
Waichayan <junior.inw@hotmail.com>
2020-05-09 00:27:06 +0700

    initial commit

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
-    public static void public static void main(String[] args) {
-    system.out.println("kuy");
-        
+    public static void main(String[] args) {
+    system.out.println("Hello");
+
     }
         
     
