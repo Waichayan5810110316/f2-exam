962ebd386282444a0400ec7cf95b4ef9b9904f86
Waichayan <waichayan.nuisawee@gmail.com>
2020-05-09 14:26:10 +0700

    initial commit

--- Final.java
+++ Final.java
@@ -11,6 +11,7 @@ public class Final {
         Scanner sc = new Scanner(new File("C:\\Users\\User\\Desktop\\f2-exam\\file.txt"));
            
             while(sc.hasNextLine()){
+            String key = "" ;
             String currentline = "";
             double w = 0;
             double r = 0;
@@ -26,19 +27,22 @@ public class Final {
             if(currentline.contains("W")){ 
                indexw =  currentline.lastIndexOf("W"); 
                w = Double.parseDouble(currentline.substring(indexw+1, indexw+3));
+               //System.out.println(w);
                 if(currentline.contains("H")){
                    
                     indexh =  currentline.lastIndexOf("H");
                    // System.out.println(currentline.substring(indexw+1, indexw+3));
                   
                     h = Double.parseDouble(currentline.substring(indexh+1, indexh+3));
+                   // System.out.println(h);
                     if(currentline.contains("D")){
                         
                       
-                        indexd =  currentline.lastIndexOf("D");
-                        System.out.println(currentline.substring(indexd+1, indexd+2));
+                        indexd =  currentline.indexOf("D");
+                      // System.out.println(currentline.substring(indexd+2, indexd+3));
                         
-                        d = Double.parseDouble(currentline.substring(indexd+1, indexw+3));
+                        d = Double.parseDouble(currentline.substring(indexd+2, indexd+3));
+                        //System.out.println(indexd);
                         shape3ds.add(new Cuboid(currentline.substring(0,1),w,h,d));
                         System.out.println("cuboid w = "+ w +"h ="+ h + "d = "+ d);
                     }
@@ -57,7 +61,7 @@ public class Final {
               // System.out.println(currentline.substring(indexr+1, indexr+3));
 
                //System.out.println(indexr);
-               if(indexr > 2){
+               if(indexr > 7){
                    System.out.println("Sphere r ="+ r);
                    shape3ds.add(new Sphere(currentline.substring(0,1),r));
                }
@@ -73,20 +77,29 @@ public class Final {
                    shape2ds.add(new Circle(currentline.substring(0,1),r));
                }
             }
-            for(Shape2D s : shape2ds){
-                System.out.println(s.getName());
-            }
-            for(Shape3D s : shape3ds){
-                System.out.println(s.getName());
-            }
+            
 
-
+            
            
         }
         }catch(FileNotFoundException e){
             System.out.println("File not Found");
         }catch(Exception exception){
             System.out.println("something Wrong!!!");
-        }        
+        }for(Shape2D s : shape2ds){
+                System.out.println(s.getName());
+            }
+            for(Shape3D s : shape3ds){
+                System.out.println(s.getName());
+            }
+        Scanner s2 = new Scanner(System.in);
+        String key = s2.nextLine();
+        
+        for(Shape2D s : shape2ds){
+            int totalarea = 0;
+            if(s.getName()==key){
+
+            }
+        }
     }
 }
