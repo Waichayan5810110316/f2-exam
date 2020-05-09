52129b67414cd7bcf59fd3f092b347c0389a9cf5
Waichayan <waichayan.nuisawee@gmail.com>
2020-05-09 11:55:59 +0700

    initial commit

--- Cuboid.java
+++ Cuboid.java
@@ -1,3 +1,55 @@
-public class Cuboid {
+public class Cuboid extends Shape3D{
+
+    private double area;
+    private double width;
+    private double height;
+    private double depth;
+    private double volumn;
+
+    public Cuboid(String name, double width ,double height,double depth){
     
+        super(name);
+        this.width = width;
+        this.height = height;
+        this.depth = depth;       
+    }
+
+    public double getwidth(){
+        return width;
+    }
+
+    public void setwidth(double width){
+        this.width = width;
+    }
+
+    public double getHeigth(){
+        return width;
+    }
+
+    public void setHeight(double height){
+        this.height = height;
+    }
+
+    public double getdepth(double depth){
+        return depth;
+    }
+
+    public void setdepth(double depth ){
+        this.depth = depth;
+    }
+
+
+
+
+    public double getArea(){
+        area = (depth * height * 2) + (2 * width * height) + (2 * width * depth);
+        return area;
+    }
+    
+    public double getVolumn(){
+        volumn =  width * depth * height;
+        return volumn;
+    }
+    
+
 }
--- Final.java
+++ Final.java
@@ -1,15 +1,18 @@
+import java.io.FileNotFoundException;
+import java.io.File;
 import java.util.*;
 
 public class Final {
     public static void main(String[] args) {
-        
+ 
+        try{
+        Scanner sc = new Scanner(new File("C:\\Users\\User\\Desktop\\f2-exam\\file.txt"));
+           String currentline = sc.nextLine();
+           System.out.println(currentline);
 
 
-
-
-
-
-        
+        }catch(FileNotFoundException e){
+            System.out.println("File not Found");
+        }        
     }
-    
 }
